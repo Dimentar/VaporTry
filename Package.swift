aabefd8a91e71f98247c30622f73b520402edbd5
Dimentar <dimentar@gmail.com>
2020-04-14 22:41:47 +0200

    package: removed Tagged

--- Package.swift
+++ Package.swift
@@ -15,14 +15,12 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc.1"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc.1"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0-rc.1"),
-        .package(name: "Tagged", url: "https://github.com/pointfreeco/swift-tagged.git", from: "0.5.0")
     ],
     targets: [
         .target(name: "App", dependencies: [
             .product(name: "Fluent", package: "fluent"),
             .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
             .product(name: "Vapor", package: "vapor"),
-            .product(name: "Tagged", package: "Tagged"),
         ]),
         .target(name: "Run", dependencies: [
             .target(name: "App"),
